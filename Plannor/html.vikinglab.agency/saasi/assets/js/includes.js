578e0cee68e8e1a1d0e7dbd4cf64d7d0e3678768
Your Name <dakshkathayat7@gmail.com>
2026-06-19 09:37:44 +0530

    Fix mobile hamburger menu - move to right, fix offcanvas toggle

--- Plannor/html.vikinglab.agency/saasi/assets/js/includes.js
+++ Plannor/html.vikinglab.agency/saasi/assets/js/includes.js
@@ -20,9 +20,24 @@
                 if (!el) return;
                 var tmp = document.createElement('div');
                 tmp.innerHTML = html;
+
+                // Collect inline scripts before moving nodes
+                var scripts = [];
+                tmp.querySelectorAll('script').forEach(function (s) {
+                    scripts.push(s.textContent);
+                    s.parentNode.removeChild(s);
+                });
+
                 var frag = document.createDocumentFragment();
                 while (tmp.firstChild) frag.appendChild(tmp.firstChild);
                 el.parentNode.replaceChild(frag, el);
+
+                // Execute collected scripts now that DOM is updated
+                scripts.forEach(function (code) {
+                    var s = document.createElement('script');
+                    s.textContent = code;
+                    document.head.appendChild(s);
+                });
             });
     }
 
